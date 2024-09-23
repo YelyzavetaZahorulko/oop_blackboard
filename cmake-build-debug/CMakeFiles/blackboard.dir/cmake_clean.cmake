72ed365f201f866cfdaecadb22abbfa9c33e5ed1
yelyzaveta <yzahorulko@kse.org.ua>
2024-09-23 22:24:16 +0300

    add [oO]bj [bB]in to the gitignore file, because before send a lot of trash to github

---
+++ cmake-build-debug/CMakeFiles/blackboard.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blackboard.dir/main.cpp.o"
+  "CMakeFiles/blackboard.dir/main.cpp.o.d"
+  "blackboard"
+  "blackboard.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blackboard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/blackboard.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for blackboard.
+# This may be replaced when dependencies are built.
